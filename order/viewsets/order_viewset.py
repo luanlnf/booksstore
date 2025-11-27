6231b4e329e0ea36db1d5e707ce11e4bb69d602f
LUAN FERREIRA <130109744+luanlnf@users.noreply.github.com>
2025-11-27 11:19:48 -0300

    feat: add build workflow

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -3,22 +3,11 @@ from rest_framework.viewsets import ModelViewSet
 
 from order.models.order import Order
 from order.serializers import OrderSerializer
-from rest_framework.authentication import (
-    SessionAuthentication,
-    BasicAuthentication,
-    TokenAuthentication,
-)
-from rest_framework.permissions import IsAuthenticated
+
+
 
 
 class OrderViewSet(ModelViewSet):
-    authentication_classes = [
-        SessionAuthentication,
-        BasicAuthentication,
-        TokenAuthentication,
-    ]
-    permission_classes = [IsAuthenticated]
-
     serializer_class = OrderSerializer
     queryset = Order.objects.all().order_by("id")
 
